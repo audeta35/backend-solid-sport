704e0fc6dad8c7e8142f5b2443f531466c93bb0c
audeta35 <audeta35@gmail.com>
2020-07-10 00:07:23 +0700

    dev#15: Restful api configuration

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
-const app = require('http').Server(express());
+const server = express();
+const app = require('http').Server(server);
+const indexRoutes = require('./routes/index');
+const cors = require('cors');
+
+// const whiteList = [
+//   "http://localhost:3000",
+//   "http://localhost:4200",
+//   "http://localhost:4300",
+// ];
+
+// // CORS
+// server.use(
+//   cors({
+//     origin: (origin, cb) => {
+//       if (whiteList.indexOf(origin) !== -1) {
+//         cb(null, true);
+//       } else {
+//         cb(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+//   })
+// );
+
+// Routes
+server.use('/', indexRoutes);
 
 app.listen(port);
 console.log(`Server listening on port ${port}`);
