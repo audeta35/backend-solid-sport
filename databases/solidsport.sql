13cff6b4c6a5abadcae3e9aeb97735ac38793bff
audeta35 <audeta35@gmail.com>
2020-07-12 16:52:31 +0700

    master: add database solidsport.sql

--- databases/solidsport.sql
+++ databases/solidsport.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 08 Jul 2020 pada 17.18
+-- Waktu pembuatan: 12 Jul 2020 pada 11.51
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.3.18
 
@@ -87,10 +87,28 @@ CREATE TABLE `result` (
 
 CREATE TABLE `users` (
   `id_user` int(11) NOT NULL,
+  `token` text NOT NULL,
+  `tatami` varchar(21) NOT NULL,
+  `name` varchar(21) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` text NOT NULL,
-  `level` int(11) NOT NULL
+  `level` int(11) NOT NULL,
+  `status` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id_user`, `token`, `tatami`, `name`, `username`, `password`, `level`, `status`) VALUES
+(1, '', 'aka', 'j1', 'Tiger', 'supratman', 0, 'online'),
+(3, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VycyI6W3siaWRfdXNlciI6MywidG9rZW4iOiJleUpoYkdjaU9pSklVekkxTmlJc0luUjVjQ0k2SWtwWFZDSjkuZXlKMWMyVnljeUk2VzNzaWFXUmZkWE5sY2lJNk15d2lkRzlyWlc0aU9pSWlMQ0owWVhSaGJXa2lPaUlpTENKdVlXMWxJam9pWVdSdGFXNGlMQ0oxYzJWeWJtRnRaU0k2SW1GMVpHVjBZU0lzSW5CaGMzTjNiM0prSWpvaVlYVmtaWFJoSWl3aWJHVjJaV3dpT2pFc0luTjBZWFIxY3lJNkltOW1abXhwYm1VaWZWMHNJbWxoZENJNk1UVTVORFV5TVRrNE5pd2laWGh3SWpveE5UazBOVFkxTVRnMmZRLmwwWTMyQkdzY0RhZ19sQUpUM1QyQzB4RjBuNll1TW5lWk1XX3RuZDI2cFEiLCJ0YXRhbWkiOiIiLCJuYW1lIjoiYWRtaW4iLCJ1c2VybmFtZSI6ImF1ZGV0YSIsInBhc3N3b3JkIjoiYXVkZXRhIiwibGV2ZWwiOjEsInN0YXR1cyI6Im9ubGluZSJ9XSwiaWF0IjoxNTk0NTMxMjUwLCJleHAiOjE1OTQ1NzQ0NTB9.GhJGmegcYhe5CowiDsfyfMy1wnKmWqX7lu0RgKMi_6g', 'aka', 'j7', 'Flak Panzer', 'audeta', 1, 'online'),
+(4, '', 'aka', 'j3', 'Leopard', 'asd', 0, 'offline'),
+(5, '', 'aka', 'j4', 'Panzer 4', 'dsadsa', 0, 'offline'),
+(6, '', 'ao', 'j5', 'Maus', 'asdasdasd', 0, 'offline'),
+(7, '', 'ao', 'j6', 'Panther', 'panther', 0, 'offline'),
+(8, '', 'aka', 'j7', 'Flak Panzer', 'flakpanser', 0, 'offline'),
+(10, '', 'ao', 'j2', 'a7v', 'a7v', 0, 'offline');
 
 --
 -- Indexes for dumped tables
@@ -156,7 +174,7 @@ ALTER TABLE `result`
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
